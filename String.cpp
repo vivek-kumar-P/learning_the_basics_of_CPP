406aa4e0e1074077c5cf55a6577337cc89f60d1d
vivek-kumar-P <vkprajapati529@gmail.com>
2025-01-29 20:28:37 +0530

    reverse of a string, size of a string, pallindrom checker

---
+++ String.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // string s;
+    // getline(cin,s);
+    // cout<<s<<endl;
+    // cout<<s.size();
+
+    // string s1 = "rohit",s2 = "mohit";
+    // s1 += s2;
+    // cout << s1;
+
+    // string s1 = "rohit",s2 = "mohit";
+    // s1.push_back('p');
+    // cout << s1;
+
+    // string s1 = "rohit",s2 = "mohit";
+    // s1.pop_back();
+    // cout << s1;
+
+    // string s = "rohit is a \"good\" boy";
+    // cout << s;
+
+
+// reverse of a string
+
+
+    // int start = 0, end = s.size()-1;
+    // while(start < end){
+    //     swap(s[start],s[end]);
+    //     start++,end--;
+    // }
+    // cout << s;
+
+// calculate the size of a string manually
+
+    // string s = "rohit is a \"good\" boy";
+    // int size = 0;
+    // while(s[size] != '\0'){
+    //     size++;
+    // }
+    // cout << size;
+
+
+// code to find wheater a string is a pallindrom or not 
+    // string s2 = "naman";
+    // int start = 0, end = s2.size()-1;
+    // while (start < end){
+    //     if(s2[start]!= s2[end]){
+    //         cout << "not a pallindrom";
+    //     }
+    //     start++,end--;
+    // }
+    // cout << "the string is a pallindrom";
+
+}
